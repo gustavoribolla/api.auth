55660dcc760610ba0d9b0aba60f0cc0909fc9f63
ribollequis87 <gustavocr2@al.insper.edu.br>
2025-06-03 21:54:08 -0300

    fix: sintaxe de records corrigida

--- src/main/java/store/auth/SolveOut.java
+++ src/main/java/store/auth/SolveOut.java
@@ -1,14 +1,11 @@
 package store.auth;
 
 import java.io.Serializable;
-
 import lombok.Builder;
 import lombok.experimental.Accessors;
 
-@Builder @Accessors(fluent = true)
-public record SolveOut(
-    private static final long serialVersionUID = 1L;
-    String idAccount
-) implements Serializable {
+@Builder
+@Accessors(fluent = true)
+public record SolveOut(String idAccount) implements Serializable {
     private static final long serialVersionUID = 1L;
 }
--- src/main/java/store/auth/TokenOut.java
+++ src/main/java/store/auth/TokenOut.java
@@ -1,15 +1,11 @@
 package store.auth;
 
 import java.io.Serializable;
-
 import lombok.Builder;
 import lombok.experimental.Accessors;
 
-@Builder @Accessors(fluent = true)
-public record TokenOut(
-    private static final long serialVersionUID = 1L;
-    String id,
-    String token
-) implements Serializable {
+@Builder
+@Accessors(fluent = true)
+public record TokenOut(String id, String token) implements Serializable {
     private static final long serialVersionUID = 1L;
 }
